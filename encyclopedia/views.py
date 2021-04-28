5f4f58ca3630499f3517fe832728d053c74e4cce
adunlop1234 <dunlop.arthur@gmail.com>
2021-04-28 20:44:55 +0100

    Added entry page functionality and markdown to html conversion for pages.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
 from . import util
-
+from markdown2 import Markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    if title in util.list_entries():
+        markdowner = Markdown()
+        print(markdowner.convert(util.get_entry(title)))
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": markdowner.convert(util.get_entry(title))
+        })
+    else:
+        return render(request, "encyclopedia/entryerror.html", {
+            "title": title
+        })
